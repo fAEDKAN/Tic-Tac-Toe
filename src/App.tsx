ac873043c9f45f0b1aad279e83e4d327c858ffad
fA3DKAN <103541453+fAEDKAN@users.noreply.github.com>
2024-04-25 22:33:00 -0300

    remove tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 export default function Square() {
   return (
     <>
-      <button className="square">X</button>
+      <div>
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div>
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div>
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
     </>
   );
 }
